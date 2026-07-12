d0c2f888be10b50f95e2c0afcf1a986c011ad729
sulaimon <sulele04@gmail.com>
2026-07-12 13:15:35 +0100

    - creating the map functionality

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-import (
-	"fmt"
-)
+import "errors"
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+var ErrorNonExistentKey = errors.New("key doesn't exist")
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+var store = map[string]string{}
+
+func Put(key, value string) error {
+	store[key] = value
+
+	return nil
+}
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+	if !ok {
+		return "", ErrorNonExistentKey
 	}
+	return value, nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+	return nil
+}
+
+func main() {
+
 }
